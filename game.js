80050d6149d6c1fe7dc6bd605cb90c2fade3a7b4
Bartlomiej <bartlomiejbartosiewicz@wp.pl>
2019-11-19 13:41:39 +0100

    add js code - game.js

--- game.js
+++ game.js
@@ -1,1 +1,21 @@
-// Zobacz gotowy projekt: https://websamuraj.pl/examples/js/projekt7/
+const gameStats = {
+    games: 0,
+    wins: 0,
+    lose: 0,
+    draws: 0,
+};
+const game = {
+    playerHand: "",
+    aiHand: "",
+};
+
+const imgsGame = [...document.querySelectorAll(" .select img")];
+
+imgsGame.forEach((oneImg) => {
+    oneImg.addEventListener("click", (e) => {
+        game.playerHand = e.target.dataset.option;
+        imgsGame.forEach(img => img.style.boxShadow = "");
+        e.target.style.boxShadow = "0 0 10px 10px rgb(235, 119, 10)";
+        console.log(e.target.dataset.option);
+    });
+});
