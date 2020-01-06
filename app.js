ec2f405d513437f4f953398536abe0a4fa394aac
Bartlomiej <bartlomiejbartosiewicz@wp.pl>
2020-01-06 17:30:33 +0100

    update

--- app.js
+++ app.js
@@ -20,7 +20,6 @@ class App extends React.Component {
         })
     }
     render() {
-        const defText1 = "dane";
         const p1Text1 = `Zakończenie montażu - 10 kwietnia 1984`;
         const p2Text1 = `Masa - 68 ton`;
         const p3Text1 = `Długość: 37,2 m`;
@@ -28,16 +27,14 @@ class App extends React.Component {
         const p5Text1 = `Rozpiętość skrzydeł: 23,7 m`;
         const p6Text1 = `Całkowita długość przy starcie wraz z pomocniczym silnikiem na paliwo stałe - 56 m`;
         const p7Text1 = `Napęd: trzy silniki. Moc każdego z nich to ponad 12 milionów koni mechanicznych.`;
-        const defText2 = "historia";
         const p1Text2 = `Pierwszy start promu Atlantis miał miejsce 3 października 1985 roku. Wahadłowiec dostarczył na orbitę objęty tajemnicą ładunek Departamentu Obrony USA. Była to jedna z pięciu tajnych misji związanych z programem wojen gwiezdnych. W roku 1989 Atlantis wyniósł na swoim pokładzie sondy kosmiczne Magellan oraz Galileo, w roku 1991 Teleskop kosmiczny Comptona, w roku 2008 laboratorium kosmiczne Columbus – Moduł ISS skonstruowany przez ESA. W 2009 wykonał ostatnią misję serwisową do Teleskopu Hubble’a (STS-125), a w 2011 ostatni lot w programie wahadłowców (STS-135).`;
-        const defText3 = "Ciekawostki"
         const p1Text3 = `Prom kosmiczny Atlantis (czwarty wahadłowiec NASA) wykonał 33 misje, spędził w przestrzeni 306 dni, zaliczył 4848 orbit, przeleciał łącznie 203 mln km, liczba astronautów na pokładzie: 191, liczba misji do stacji orbitalnej Mir: 7, liczba misji do ISS: 12.`;
         return (
             <>
                 <h2>Prom kosmiczny atlantis</h2>
-                <h2 onClick={this.handleClickTittle1} class="point">{this.state.text1Active ? defText1 : <><p>{p1Text1}</p><p>{p2Text1}</p><p>{p3Text1}</p><p>{p4Text1}</p> <p>{p5Text1}</p><p>{p6Text1}</p><p>{p7Text1}</p></>}</h2>
-                <h2 onClick={this.handleClickTittle2} class="point">{this.state.text2Active ? defText2 : <p>{p1Text2}</p>}</h2>
-                <h2 onClick={this.handleClickTittle3} class="point">{this.state.text3Active ? defText3 : <p>{p1Text3}</p>}</h2>
+                <h2 onClick={this.handleClickTittle1} class="point">{this.state.text1Active ? "dane" : <><p>{p1Text1}</p><p>{p2Text1}</p><p>{p3Text1}</p><p>{p4Text1}</p> <p>{p5Text1}</p><p>{p6Text1}</p><p>{p7Text1}</p></>}</h2>
+                <h2 onClick={this.handleClickTittle2} class="point">{this.state.text2Active ? "historia" : <p>{p1Text2}</p>}</h2>
+                <h2 onClick={this.handleClickTittle3} class="point">{this.state.text3Active ? "ciekawostki" : <p>{p1Text3}</p>}</h2>
             </>
         )
     }
