eb4a28c31745aedb100f2daa6f258721c47ed5d3
Bartlomiej <bartlomiejbartosiewicz@wp.pl>
2020-01-27 10:44:36 +0100

    change project

---
+++ precache-manifest.f18549bcf4e303e39febfb6963dbc23c.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a86908c325e45f0f96ceebc7a6e3cb7",
+    "url": "/test/index.html"
+  },
+  {
+    "revision": "74552f4fcd439559f810",
+    "url": "/test/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "e04d46887070f9e2acf3",
+    "url": "/test/static/css/main.92010ba4.chunk.css"
+  },
+  {
+    "revision": "74552f4fcd439559f810",
+    "url": "/test/static/js/2.de754fb0.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/test/static/js/2.de754fb0.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e04d46887070f9e2acf3",
+    "url": "/test/static/js/main.99dc975e.chunk.js"
+  },
+  {
+    "revision": "a81169e25570446ed3b0",
+    "url": "/test/static/js/runtime-main.0c97cd2c.js"
+  },
+  {
+    "revision": "50100195cc58de235157b136d6a0f2a8",
+    "url": "/test/static/media/bazylia.50100195.jpg"
+  },
+  {
+    "revision": "ec31484c7b1547d50cf64e63cbdcc9ac",
+    "url": "/test/static/media/brokul.ec31484c.jpg"
+  },
+  {
+    "revision": "a7f9000ec3816c7593d8f702854f0960",
+    "url": "/test/static/media/czosnek.a7f9000e.jpg"
+  },
+  {
+    "revision": "e9add1f6f3c44537af077a354ab6272d",
+    "url": "/test/static/media/gruszka.e9add1f6.jpg"
+  },
+  {
+    "revision": "1d041799ff8de7c25875bb5fb623629b",
+    "url": "/test/static/media/jagoda.1d041799.jpg"
+  },
+  {
+    "revision": "8a99d59227d8825cf2669c7aadfc1075",
+    "url": "/test/static/media/logo.8a99d592.png"
+  },
+  {
+    "revision": "e796be189b4d26864e828a9e5930c347",
+    "url": "/test/static/media/poziomka.e796be18.jpg"
+  },
+  {
+    "revision": "fed69c4ab452e6a7e6a876d153aa88df",
+    "url": "/test/static/media/sklep.fed69c4a.jpg"
+  }
+]);
