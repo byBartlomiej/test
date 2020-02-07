94b6d764647d4480c5a39f010a952510a3c3a320
Bartlomiej <bartlomiejbartosiewicz@wp.pl>
2020-02-07 20:55:58 +0100

    add custom events into project

--- main.js
+++ main.js
@@ -1,4 +1,7 @@
 document.addEventListener('DOMContentLoaded', () => {
     const scroller = new Scroller('scroller');
     document.addEventListener('wheel', (e) => scroller.scrollPage(e));
+
+    document.addEventListener('swipeUp', () => scroller.scrollDirection(1))
+    document.addEventListener('swipeDown', () => scroller.scrollDirection(-1))
 });
--- swiper.js
+++ swiper.js
@@ -2,23 +2,45 @@ class Swiper {
     constructor() {
         this.initialX = null;
         this.initialY = null;
-        document.addEventListener('touchstart', this.startTouch);
-        document.addEventListener('touchmove', this.startMove);
+        document.addEventListener('touchstart', (e) => this.startTouch(e));
+        document.addEventListener('touchmove', (e) => this.startMove(e));
+
+        this.events = {
+            swipeUp: new Event('swipeUp'),
+            swipeDown: new Event('swipeDown'),
+            swipeLeft: new Event('swipeLeft'),
+            swipeRight: new Event('swipeRight'),
+        }
     }
 
     startTouch(e) {
-        e.preventDefult();
+        e.preventDefault();
         this.initialX = e.touches[0].clientX;
         this.initialY = e.touches[0].clientY;
     }
-    startTouch(e) {
+    startMove(e) {
         if (!this.initialX || !this.initialY) return;
         const currentX = e.touches[0].clientX;
         const currentY = e.touches[0].clientY;
 
         const diffX = this.initialX - currentX;
-        const diffy = this.initialY - currentY;
+        const diffY = this.initialY - currentY;
 
+        if (Math.abs(diffX) > Math.abs(diffY)) {
+            if (diffX > 0) {
+                document.dispatchEvent(this.events.swipeLeft);
+            } else {
+                document.dispatchEvent(this.events.swipeRight);
+            }
+        } else {
+            if (diffY > 0) {
+                document.dispatchEvent(this.events.swipeUp);
+            } else {
+                document.dispatchEvent(this.events.swipeDown);
+            }
+        }
+        this.initialX = null;
+        this.initialY = null;
     }
 }
 new Swiper();
