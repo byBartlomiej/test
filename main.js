fd9c74c1aafda78b0fb4bee7e278366787cdae93
Bartlomiej <bartlomiejbartosiewicz@wp.pl>
2020-02-07 21:04:54 +0100

    add new event - main.js

--- main.js
+++ main.js
@@ -2,6 +2,18 @@ document.addEventListener('DOMContentLoaded', () => {
     const scroller = new Scroller('scroller');
     document.addEventListener('wheel', (e) => scroller.scrollPage(e));
 
-    document.addEventListener('swipeUp', () => scroller.scrollDirection(1))
-    document.addEventListener('swipeDown', () => scroller.scrollDirection(-1))
+    document.addEventListener('swipeUp', () => scroller.scrollDirection(1));
+    document.addEventListener('swipeDown', () => scroller.scrollDirection(-1));
+
+    document.addEventListener('keydown', (e) => {
+        switch (e.keyCode) {
+            case 40:
+                return scroller.scrollDirection(1);
+            case 38:
+                return scroller.scrollDirection(-1);
+            default:
+                return;
+        }
+
+    })
 });
