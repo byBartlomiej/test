096cff6b01608ff0ae716ffafc03d8b1d840dadb
Bartlomiej <bartlomiejbartosiewicz@wp.pl>
2020-02-12 12:23:42 +0100

    change project on todo list

---
+++ precache-manifest.49c4f4502f0f8a5b8d6f06f89f75a6fd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c1206f2f9fec3230cc5d9341030058a0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "53fced6e38fb0865de0e",
+    "url": "/static/css/main.3a9c9574.chunk.css"
+  },
+  {
+    "revision": "262a1bad9ae42f154c2e",
+    "url": "/static/js/2.0ef83296.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.0ef83296.chunk.js.LICENSE"
+  },
+  {
+    "revision": "53fced6e38fb0865de0e",
+    "url": "/static/js/main.3f2555d8.chunk.js"
+  },
+  {
+    "revision": "00b67eaf620f014cd0ac",
+    "url": "/static/js/runtime-main.13290678.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/test/precache-manifest.2b6f1fe12e01ef0986101122915a9957.js"
+  "/precache-manifest.49c4f4502f0f8a5b8d6f06f89f75a6fd.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/test/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
